870b007815aec6a62fbcb5c91bdd62f4acecebc6
TrellixVulnTeam <kasimir.schulz@trellix.com>
2022-10-02 20:51:04 +0000

    Adding tarfile member sanitization to extractall()

--- src/mbio/tools/whole_transcriptome/transfer.py
+++ src/mbio/tools/whole_transcriptome/transfer.py
@@ -67,7 +67,26 @@ class TransferTool(Tool):
             if fname.endswith('.tar.gz'):
                 self.logger.debug('start decompressing {}'.format(fname))
                 with tarfile.open(fname, 'r:gz') as tar:
-                    tar.extractall(self.output_dir)
+                    def is_within_directory(directory, target):
+                        
+                        abs_directory = os.path.abspath(directory)
+                        abs_target = os.path.abspath(target)
+                    
+                        prefix = os.path.commonprefix([abs_directory, abs_target])
+                        
+                        return prefix == abs_directory
+                    
+                    def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+                    
+                        for member in tar.getmembers():
+                            member_path = os.path.join(path, member.name)
+                            if not is_within_directory(path, member_path):
+                                raise Exception("Attempted Path Traversal in Tar File")
+                    
+                        tar.extractall(path, members, numeric_owner) 
+                        
+                    
+                    safe_extract(tar, self.output_dir)
                 self.logger.debug('succeed in decompressing {}'.format(fname))
         os.chdir(self.work_dir)
         self.logger.warn('succeed in decompressing compressed file')
